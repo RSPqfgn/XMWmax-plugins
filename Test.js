7763152089ac2458bc6386caf14be58c2d5015c9
RSPqfgn <87849284+RSPqfgn@users.noreply.github.com>
2025-08-02 02:36:52 +0800

    创建 Test.js

---
+++ Test.js
@@ -0,0 +1,10 @@
+{
+    name: '示例插件',
+    version: '1.0.0',
+    author: '开发者',
+    description: '这是一个示例插件，展示了插件的基本结构',
+    enabled: true,
+    init: function() {
+        alert('示例插件已加载！');
+    }
+}
